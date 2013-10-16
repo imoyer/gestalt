c458f2c59791e846ba06171fdd43a419faac0186
Nadya Peek <nadya@infosyncratic.nl>
2013-10-16 18:53:35 +0400

    added stuff to gigapan.py which apparently wasn't checked in before anyway oops sorry

--- examples/gigapan/gigapan.py
+++ examples/gigapan/gigapan.py
@@ -2,6 +2,8 @@
 
 PORT = '/dev/ttyUSB0'
 HEXFILE = '../../../086-005/086-005a.hex'
+# tune this depending on how many frames are captured during the move
+NUMCAMERAREADS = 15
 
 #------IMPORTS-------
 from gestalt import nodes
@@ -14,7 +16,8 @@ from gestalt.machines import state
 from gestalt.utilities import notice
 from gestalt.publish import rpc	#remote procedure call dispatcher
 import time
-
+import cv2.cv as cv
+import sys
 
 #------VIRTUAL MACHINE------
 class virtualMachine(machines.virtualMachine):
@@ -33,8 +36,8 @@ class virtualMachine(machines.virtualMachine):
 		self.position = state.coordinate(['mm','mm','mm'])	#X,Y,Z
 	
 	def initKinematics(self):
-		self.xAxis = elements.elementChain.forward([elements.microstep.forward(4), elements.stepper.forward(1.8), elements.leadscrew.forward(2), elements.invert.forward(False)])
-		self.yAxis = elements.elementChain.forward([elements.microstep.forward(4), elements.stepper.forward(1.8), elements.leadscrew.forward(2), elements.invert.forward(False)])
+		self.xAxis = elements.elementChain.forward([elements.microstep.forward(4), elements.stepper.forward(1.8), elements.leadscrew.forward(2), elements.invert.forward(True)])
+		self.yAxis = elements.elementChain.forward([elements.microstep.forward(4), elements.stepper.forward(1.8), elements.leadscrew.forward(2), elements.invert.forward(True)])
 		self.zAxis = elements.elementChain.forward([elements.microstep.forward(4), elements.stepper.forward(1.8), elements.leadscrew.forward(2), elements.invert.forward(False)])
 
 		self.stageKinematics = kinematics.direct(3)	#direct drive on all three axes
@@ -85,19 +88,43 @@ if __name__ == '__main__':
 	fileReader = rpc.fileRPCDispatch()
 	fileReader.addFunctions(('move',gigapan.move), ('jog', gigapan.jog))	#expose these functions on the file reader interface.
 
-	# remote procedure call initialization
-	rpcDispatch = rpc.httpRPCDispatch(address = '0.0.0.0', port = 27272)
-	notice(gigapan, 'Started remote procedure call dispatcher on ' + str(rpcDispatch.address) + ', port ' + str(rpcDispatch.port))
-	rpcDispatch.addFunctions(('move',gigapan.move),
-				('position', gigapan.getPosition),
-				('jog', gigapan.jog),
-				('disableMotors', gigapan.xyzNode.disableMotorsRequest),
-				('loadFile', fileReader.loadFromURL),
-				('runFile', fileReader.runFile),
-				('setPosition', gigapan.setPosition))	#expose these functions on an http interface
-	rpcDispatch.addOrigins('http://tq.mit.edu', 'http://127.0.0.1:8000')	#allow scripts from these sites to access the RPC interface
-	rpcDispatch.allowAllOrigins()
-	rpcDispatch.start()
 
+	# remote procedure call initialization
+	#rpcDispatch = rpc.httpRPCDispatch(address = '0.0.0.0', port = 27272)
+	#notice(gigapan, 'Started remote procedure call dispatcher on ' + str(rpcDispatch.address) + ', port ' + str(rpcDispatch.port))
+	#rpcDispatch.addFunctions(('move',gigapan.move),
+	#			('position', gigapan.getPosition),
+	#			('jog', gigapan.jog),
+	#			('disableMotors', gigapan.xyzNode.disableMotorsRequest),
+	#			('loadFile', fileReader.loadFromURL),
+	#			('runFile', fileReader.runFile),
+	#			('setPosition', gigapan.setPosition))	#expose these functions on an http interface
+	#rpcDispatch.addOrigins('http://tq.mit.edu', 'http://127.0.0.1:8000')	#allow scripts from these sites to access the RPC interface
+	#rpcDispatch.allowAllOrigins()
+	#rpcDispatch.start()
+
+	cv.NamedWindow("camera", 1)
+
+	capture = cv.CaptureFromCAM(2)
+
+	gigapan.jog([0,0,-0.1])
+	time.sleep(1)
+	sys.exit()
+
+	while True:
+		for i in range(NUMCAMERAREADS):
+    			img = cv.QueryFrame(capture)
+    		cv.ShowImage("camera", img)
+		gigapan.jog([-1.2,0,0])
+		gigapan_status = gigapan.xAxisNode.spinStatusRequest()
+		while gigapan_status['stepsRemaining'] > 0:
+			time.sleep(0.1)
+			gigapan_status = gigapan.xAxisNode.spinStatusRequest()
+			# don't stall the UI while waiting
+    			if cv.WaitKey(10) == 27:
+        			break
+
+    		if cv.WaitKey(10) == 27:
+        		break
 
 
