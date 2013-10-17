2050f0340b3dbde208d0a01d7ccf4e8ec41b32d1
Nadya Peek <nadya@infosyncratic.nl>
2013-10-17 14:41:47 +0400

    added image capture to gigapan.py, now it can save camera images

--- examples/gigapan/gigapan.py
+++ examples/gigapan/gigapan.py
@@ -18,6 +18,7 @@ from gestalt.publish import rpc	#remote procedure call dispatcher
 import time
 import cv2.cv as cv
 import sys
+import random
 
 #------VIRTUAL MACHINE------
 class virtualMachine(machines.virtualMachine):
@@ -68,62 +69,75 @@ class virtualMachine(machines.virtualMachine):
 
 	def autofocus(self):
 		pass
-
+		
 	def takePhoto(self):
+		for i in range(NUMCAMERAREADS):
+		#the buffer of images needs to be constantly emptied or you get an old image
+    			img = cv.QueryFrame(capture)
+    		cv.ShowImage("camera", img)
+		cv.SaveImage("hi"+str(random.random())[2:]+".jpeg", img)
+		
+	def takeGigapan(self, vm, capture):
+		vm.jog([1.2,0,0])
+		#wait for the motor to be still
+		vm_status = vm.xAxisNode.spinStatusRequest()
+		while vm_status['stepsRemaining'] > 0:
+			time.sleep(0.1)
+			vm_status = vm.xAxisNode.spinStatusRequest()
+			# don't stall the UI while waiting
+    			if cv.WaitKey(10) == 27:
+        			break
+		self.takePhoto()
+		
+		
+
+class gigapan():
+	'''A grid of images and their corresponding moves'''
+	
+	def __init__(self, x0, y0, x1, y1, image_size):
+		self.x0 = x0
+		self.y0 = y0
+		self.x1 = x1
+		self.y1 = y1		
+		self.image_size = image_size
+		self.currentx = 0
+		self.currenty = 0
+
+	def next_move(self):
 		pass
-
-	def takeGigapan(self):
-		pass
-
-
+		
 
 
 #------IF RUN DIRECTLY FROM TERMINAL------
 if __name__ == '__main__':
-	gigapan = virtualMachine(persistenceFile = "test.vmp")
-#	gigapan.xyzNode.setMotorCurrent(1.1)
-#	gigapan.xyzNode.loadProgram(HEXFILE)
-	gigapan.xyzNode.setVelocityRequest(2)
-	gigapan.xyzNode.setMotorCurrent(1)
+	gigmachine = virtualMachine(persistenceFile = "test.vmp")
+#	gigmachine.xyzNode.setMotorCurrent(1.1)
+#	gigmachine.xyzNode.loadProgram(HEXFILE)
+	gigmachine.xyzNode.setVelocityRequest(2)
+#	gigmachine.xyzNode.setMotorCurrent(1)
 	fileReader = rpc.fileRPCDispatch()
-	fileReader.addFunctions(('move',gigapan.move), ('jog', gigapan.jog))	#expose these functions on the file reader interface.
+	fileReader.addFunctions(('move',gigmachine.move), ('jog', gigmachine.jog))	#expose these functions on the file reader interface.
 
 
 	# remote procedure call initialization
+	# uncomment if you want to use the tq.mit.edu/pathfinder interface
 	#rpcDispatch = rpc.httpRPCDispatch(address = '0.0.0.0', port = 27272)
-	#notice(gigapan, 'Started remote procedure call dispatcher on ' + str(rpcDispatch.address) + ', port ' + str(rpcDispatch.port))
-	#rpcDispatch.addFunctions(('move',gigapan.move),
-	#			('position', gigapan.getPosition),
-	#			('jog', gigapan.jog),
-	#			('disableMotors', gigapan.xyzNode.disableMotorsRequest),
+	#notice(gigmachine, 'Started remote procedure call dispatcher on ' + str(rpcDispatch.address) + ', port ' + str(rpcDispatch.port))
+	#rpcDispatch.addFunctions(('move',gigmachine.move),
+	#			('position', gigmachine.getPosition),
+	#			('jog', gigmachine.jog),
+	#			('disableMotors', gigmachine.xyzNode.disableMotorsRequest),
 	#			('loadFile', fileReader.loadFromURL),
 	#			('runFile', fileReader.runFile),
-	#			('setPosition', gigapan.setPosition))	#expose these functions on an http interface
+	#			('setPosition', gigmachine.setPosition))	#expose these functions on an http interface
 	#rpcDispatch.addOrigins('http://tq.mit.edu', 'http://127.0.0.1:8000')	#allow scripts from these sites to access the RPC interface
 	#rpcDispatch.allowAllOrigins()
 	#rpcDispatch.start()
-
-	cv.NamedWindow("camera", 1)
-
-	capture = cv.CaptureFromCAM(2)
-
-	gigapan.jog([0,0,-0.1])
-	time.sleep(1)
-	sys.exit()
-
+	
+	
+	capture = cv.CaptureFromCAM(1)
 	while True:
-		for i in range(NUMCAMERAREADS):
-    			img = cv.QueryFrame(capture)
-    		cv.ShowImage("camera", img)
-		gigapan.jog([-1.2,0,0])
-		gigapan_status = gigapan.xAxisNode.spinStatusRequest()
-		while gigapan_status['stepsRemaining'] > 0:
-			time.sleep(0.1)
-			gigapan_status = gigapan.xAxisNode.spinStatusRequest()
-			# don't stall the UI while waiting
-    			if cv.WaitKey(10) == 27:
-        			break
-
+		gigmachine.takeGigapan(gigmachine, capture)
     		if cv.WaitKey(10) == 27:
         		break
 
